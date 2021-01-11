947cf23e41d51ceedf100848cc412e8dff82b80a
bipin247 <bipin247@gmail.com>
2021-01-11 15:57:07 +0000

    swagger meta data .adding descripotion on model controller and operations. data on swagger-ui.html

--- src/main/java/guru/springfamework/controllers/v1/VendorController.java
+++ src/main/java/guru/springfamework/controllers/v1/VendorController.java
@@ -4,11 +4,14 @@ import guru.springfamework.api.v1.model.VendorDTO;
 import guru.springfamework.api.v1.model.VendorListDTO;
 import guru.springfamework.domain.Vendor;
 import guru.springfamework.services.VendorService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags={"This is my Vendor Controller"})
 @RestController
 @RequestMapping(VendorController.BASE_URL)
 public class VendorController {
@@ -20,18 +23,21 @@ public class VendorController {
         this.vendorService = vendorService;
     }
 
+    @ApiOperation(value="This returns list of vendors",response=Iterable.class)
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public VendorListDTO getListOfVendors(){
         return vendorService.getAllVendor();
     }
 
+    @ApiOperation(value="This returns a vendor for a given id",response=Vendor.class)
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public VendorDTO getVendor(@PathVariable Long id){
         return vendorService.getVendorById(id);
     }
 
+    @ApiOperation(value="This saves a vendor for vendor data",response=VendorDTO.class)
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public VendorDTO createVendor(@RequestBody VendorDTO vendorDTO){
